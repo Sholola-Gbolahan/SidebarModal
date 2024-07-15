fb3048dea36e24a23521945e1fb47b81188a489c
Sholola-Gbolahan <shololagbolahan@gmail.com>
2024-07-15 14:28:39 +0100

    Sidebar Css done

--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -1,7 +1,43 @@
 import React from "react"
+import { social, links } from "./data"
+import logo from "./logo.svg"
+import { FaTimes } from "react-icons/fa"
+import { useGlobalContext } from "./Context"
 
 const Sidebar = () => {
-  return <div>Sidebar</div>
+  const { isSidebarOpen, closeSidebar } = useGlobalContext()
+  return (
+    <aside className="sidebar">
+      <div className="sidebar-header">
+        <img src={logo} alt="coding Addict" className="logo" />
+        <button className="close-btn" onClick={closeSidebar}>
+          <FaTimes />
+        </button>
+      </div>
+      <ul className="links">
+        {links.map(({ id, url, text, icon }) => {
+          return (
+            <li key={id}>
+              <a href={url}>
+                {icon}
+                {text}
+              </a>
+            </li>
+          )
+        })}
+      </ul>
+
+      <ul className="social-links">
+        {social.map(({ id, url, icon }) => {
+          return (
+            <li key={id}>
+              <a href={url}>{icon}</a>
+            </li>
+          )
+        })}
+      </ul>
+    </aside>
+  )
 }
 
 export default Sidebar
